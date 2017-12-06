07a78edc6aa39a3ffa1818224e26b01040640ec4
ersagun <ersagunyalcintepe@gmail.com>
2017-12-06 13:36:01 +0100

    schema modified

--- tweet-sse/src/main/java/com/sfeir/tweet/sse/TweetSseApplication.java
+++ tweet-sse/src/main/java/com/sfeir/tweet/sse/TweetSseApplication.java
@@ -2,8 +2,6 @@ package com.sfeir.tweet.sse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-//import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
